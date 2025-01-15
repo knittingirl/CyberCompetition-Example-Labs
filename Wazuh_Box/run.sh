82fd3c71edb5456659c19beb65a877ce5553b0f9
knittingirl <10614967+knittingirl@users.noreply.github.com>
2025-01-15 09:19:34 -0500

    Update run.sh

--- Wazuh_Box/run.sh
+++ Wazuh_Box/run.sh
@@ -1,14 +1,30 @@
 #This may take a while on the initial setup
 vagrant up
 #Occasionally, when setting up multiple labs, the existent recognized SSH fingerprint will change causing Ansible to fail. This line resets the known fingerprint just in case.
-#Comment out any lines relating to agents you do not intend to use. Also please deploy all connected agents fully first.
+#Please deploy all connected agents fully first.
 
 ssh-keygen -R 192.168.56.100
 
-ansible-playbook manager.yml -i inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
+#ansible-playbook manager.yml -i inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
 
-ansible-playbook vsftp_agent.yml -i ../VSFTP_Box/inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
+echo "Are we attaching the VSFTP lab? y/N"
 
-ansible-playbook pwnable_agent.yml -i ../Pwnable_Linux/inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
+read proceed
 
-ansible-playbook -i ../Windows_Webshell/inventory.txt wazuh_winweb.yml
+if [[ $proceed == "y" ]]; then
+        ansible-playbook vsftp_agent.yml -i ../VSFTP_Box/inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
+fi
+echo "Are we attaching the pwnable Linux lab? y/N"
+
+read proceed2
+
+if [[ $proceed2 == "y" ]]; then
+        ansible-playbook pwnable_agent.yml -i ../Pwnable_Linux/inventory.txt --extra-vars 'ansible_password=vagrant ansible_user=vagrant'
+fi
+echo "Are we attaching the Windows webshell lab? y/N"
+
+read proceed3
+
+if [[ $proceed3 == "y" ]]; then
+        ansible-playbook -i ../Windows_Webshell/inventory.txt wazuh_winweb.yml
+fi
